297df575233fe19c402f013571d7ecf61a97e8d9
sloev <johannesgj@gmail.com>
2015-08-19 12:23:33 +0000

    changed __init__ to include stuff

--- tornadoMotorAcl/__init__.py
+++ tornadoMotorAcl/__init__.py
@@ -3,3 +3,7 @@
 __author__ = 'johannes valbjørn'
 __email__ = 'johannes.valbjorn@gmail.com'
 __version__ = '0.1.1'
+
+from models Resource, ResourcePermissionPair, Group
+from validate import acl_authorize, DB_GROUPS, DB_RESOURCES, DB_PERMISSIONS
+
