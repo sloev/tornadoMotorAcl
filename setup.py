6cf7e8471102b2fd86b1dcf79e5a123aa571597a
sloev <johannesgj@gmail.com>
2015-08-19 12:24:46 +0000

    fixed mistake in init

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='tornadoMotorAcl',
-    version='0.1.2',
+    version='0.1.3',
     description="acl for python, tornado and motor, with examples",
     long_description=readme + '\n\n' + history,
     author="johannes valbjørn",
--- tornadoMotorAcl/__init__.py
+++ tornadoMotorAcl/__init__.py
@@ -2,8 +2,8 @@
 
 __author__ = 'johannes valbjørn'
 __email__ = 'johannes.valbjorn@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.3'
 
-from models Resource, ResourcePermissionPair, Group
+from models import Resource, ResourcePermissionPair, Group
 from validate import acl_authorize, DB_GROUPS, DB_RESOURCES, DB_PERMISSIONS
 
